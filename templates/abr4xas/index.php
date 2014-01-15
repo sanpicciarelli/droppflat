fecd4b68b28962f877901a9cdc48a32723d8421c
abr4xas <me@abr4xas.org>
2014-01-14 23:03:56 -0430

    se agregar manifest.appcache y se hace referencia al mismo en el template

--- templates/abr4xas/index.php
+++ templates/abr4xas/index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="en" manifest="manifest.appcache">
 <head>
 	<meta charset="utf-8" />
 	<title><?php echo($page_title); ?> | El blog de abr4xas</title>
@@ -65,9 +65,17 @@
     
         <section class="blank">
                 <div class="flash">
-                        <p><a href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.es_ES" target="_blank">CC 3.0 (by-nc-sa) </a><i class="fa fa-external-link"></i> You should follow me on: <a href="http://twitter.com/abr4xas" target="_blank"><i class="fa fa-twitter"></i></a> <a href="http://github.com/abr4xas" target="_blank"><i class="fa fa-github-alt"></i></a> <a href="<?php echo BLOG_URL; ?>rss" target="_blank"><i class="fa fa-rss"></i></a> Also, if you want you can see my projects. With <i class="fa fa-heart"></i> &amp; <a href="http://dropplets.com/" target="_blank" >Dropplets</a></p>
+                        <p><a href="http://creativecommons.org/licenses/by-nc-sa/3.0/deed.es_ES" target="_blank">CC 3.0 (by-nc-sa)</a> <i class="fa fa-external-link"></i> You should follow me on: <a href="http://twitter.com/abr4xas" target="_blank"><i class="fa fa-twitter"></i></a> <a href="http://github.com/abr4xas" target="_blank"><i class="fa fa-github-alt"></i></a> <a href="<?php echo BLOG_URL; ?>rss" target="_blank"><i class="fa fa-rss"></i></a> With <i class="fa fa-heart"></i> <a href="https://github.com/dennisschipper/Flatby" target="_blank">Flatby</a> &amp; <a href="http://dropplets.com/" target="_blank" >Dropplets</a></p>
                 </div>
         </section>    
-        <?php get_footer(); ?>    
+        <?php // get_footer(); ?> 
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	  ga('create', 'UA-18333188-1', 'abr4xas.org');
+	  ga('send', 'pageview');
+	</script>           
 </body>
 </html>
